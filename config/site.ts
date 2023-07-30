bbf7744d2895f81c5da2f2032f7e5fee2c648ad2
Roby Karti S S.KOM <Robykarti.s@gmail.com>
2023-07-30 21:51:35 +0700

    Frist

---
+++ config/site.ts
@@ -0,0 +1,22 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "D I",
+  description:
+    "Detla inovasi ",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Product",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "/",
+    github: "/",
+    docs: "/",
+  },
+}
